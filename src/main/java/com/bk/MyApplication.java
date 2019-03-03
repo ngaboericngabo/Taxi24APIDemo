cbb5cfa777e06727a3c4f21b9eb31095f788be20
ericn <ericn@crimsonlogic.com>
2019-03-03 17:27:33 +0200

    changing packaging

--- src/main/java/com/bk/MyApplication.java
+++ src/main/java/com/bk/MyApplication.java
@@ -2,6 +2,8 @@ package com.bk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -9,12 +11,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.bk.repo")
 @ComponentScan({"com.bk.domain","com.bk.service"}) 
 //@EntityScan("org.soluvas.buzz.core.jpa")
-public class MyApplication {
+public class MyApplication  extends SpringBootServletInitializer{
 	
  public static void main(String[] args) {
 	SpringApplication.run(MyApplication.class, args);
 	
 	}
  
-
+ @Override
+ protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+     return application.sources(MyApplication.class);
+ }
 }
